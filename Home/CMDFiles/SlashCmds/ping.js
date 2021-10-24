8976c423f87d1291f14db44f9cdb6e9fda853fc7
Phydose <56157625+Phydose@users.noreply.github.com>
2021-10-24 15:42:23 +0100

    Update ping.js

--- Home/CMDFiles/SlashCmds/ping.js
+++ Home/CMDFiles/SlashCmds/ping.js
@@ -3,10 +3,10 @@ module.exports = {
     description: "My ping!",
     run: async (client, interaction, Discord) => {
         const ping = new Discord.MessageEmbed()
-            .setColor('RANDOM')
+            .setColor(client.config.color)
             .setTimestamp()
             .setTitle('🏓╎Pong!')
-            .setDescription(`🏠╎Websocket Latency: ${client.ws.ping}ms\n🤖╎Bot Latency: ${Date.now() - interaction.createdTimestamp}ms`);
+            .setDescription(`🏠╎Websocket Latency: \`${client.ws.ping}\`ms\n🤖╎Bot Latency: \`${Date.now() - interaction.createdTimestamp}\`ms`);
         interaction.reply({
             embeds: [ping]
         })
